c1509aa21b451687008eb76aa6f0912a8061484b
fszok <371612495@qq.com>
2022-03-20 09:33:14 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -60,7 +60,6 @@ cat >${GITHUB_WORKSPACE}/Clear <<-EOF
 rm -rf feeds.buildinfo
 rm -rf openwrt-x86-64-generic-kernel.bin
 rm -rf openwrt-x86-64-generic.manifest
-rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
 rm -rf sha256sums
 rm -rf version.buildinfo
 EOF
